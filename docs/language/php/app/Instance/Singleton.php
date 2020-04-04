20aa46249150ab943691d0c223267c6febb0a9c6
gaotiefeng <1096392101@qq.com>
2020-04-04 14:26:56 +0800

    update instance

---
+++ docs/language/php/app/Instance/Singleton.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+/**
+ * Created by : PhpStorm
+ * User: 1096392101@qq.com
+ * Date: 2020/4/4
+ * Time: 2:14 下午
+ */
+
+namespace App\Instance;
+
+
+class Singleton
+{
+    private static $instance;
+
+    public static function getInstance() :Singleton
+    {
+        if (self::$instance === null)
+        {
+            self::$instance = new Singleton();
+        }
+
+        return self::$instance;
+    }
+
+    private function __construct()
+    {
+
+    }
+
+    private function __clone()
+    {
+        // TODO: Implement __clone() method.
+    }
+
+    private function __wakeup()
+    {
+        // TODO: Implement __wakeup() method.
+    }
+}
---
+++ docs/language/php/tests/src/Design/InstanceSingletonTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by : PhpStorm
+ * User: 1096392101@qq.com
+ * Date: 2020/4/4
+ * Time: 2:18 下午
+ */
+
+use PHPUnit\Framework\TestCase;
+use App\Instance;
+
+class InstanceSingletonTest extends TestCase
+{
+    public function testUniqueness()
+    {
+        $firstCall = Instance\Singleton::getInstance();
+        $secondCall = Instance\Singleton::getInstance();
+
+        $this->assertInstanceOf(Instance\Singleton::class, $firstCall);
+        $this->assertSame($firstCall, $secondCall);
+    }
+}
