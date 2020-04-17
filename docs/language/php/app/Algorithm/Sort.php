bf84a83f01ec81c0d84827fd9b8fe03faf081d6f
gaotiefeng <1096392101@qq.com>
2020-04-17 15:16:23 +0800

    update algorithm

---
+++ docs/language/php/app/Algorithm/Sort.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Algorithm;
+
+
+class Sort
+{
+
+    public function bubbleSort(array $arr):array
+    {
+        $length = count($arr);
+        for ($i = 0; $i < $length - 1; $i++) {
+            for ($j = 0; $j < $length - 1 - $i; $j++) {
+                if ($arr[$j] > $arr[$j+1]) {
+                    $temp = $arr[$j];
+                    $arr[$j] = $arr[$j+1];
+                    $arr[$j+1] = $temp;
+                }
+            }
+        }
+
+        return $arr;
+    }
+}
---
+++ docs/language/php/tests/src/Algorithm/SortTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\tests\src\Algorithm;
+
+
+use App\Algorithm\Sort;
+use PHPUnit\Framework\TestCase;
+
+class SortTest extends TestCase
+{
+    protected $sort;
+
+    public function setUp()
+    {
+        $this->sort = new Sort();
+    }
+
+    protected $data = [
+        28, 12, 89, 73, 65, 18, 96, 50, 8, 36
+    ];
+
+    public function testBubbleSort()
+    {
+        $res = $this->sort->bubbleSort($this->data);
+
+        $this->assertSame(8,$res[0]);
+    }
+}
